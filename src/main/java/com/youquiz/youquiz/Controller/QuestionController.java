fcdaf6132a3d3952a8ace9cf247213009dffeb40
belassiria aymane <aymanebel2@outlook.fr>
2023-11-08 15:22:08 +0100

    YQ1-13 edit a question

--- src/main/java/com/youquiz/youquiz/Controller/QuestionController.java
+++ src/main/java/com/youquiz/youquiz/Controller/QuestionController.java
@@ -6,10 +6,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -29,6 +26,34 @@ public class QuestionController {
             return new ResponseEntity<>(message, HttpStatus.CREATED);
         }catch(Exception e){
             message.put("message", "cannot create a question");
+            return new ResponseEntity<>(message, HttpStatus.NOT_ACCEPTABLE);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Map<String, Object>> updateQuestion(@PathVariable long id, @RequestBody QuestionDTO questionDTO){
+        Map<String, Object> message = new HashMap<>();
+        try{
+            message.put("message", "question updated");
+            message.put("question", questionService.update(id, questionDTO));
+            return new ResponseEntity<>(message, HttpStatus.OK);
+        }catch(Exception e){
+            System.out.println("error:"+e.getMessage());
+            message.put("message", "cannot update this question");
+            return new ResponseEntity<>(message, HttpStatus.NOT_ACCEPTABLE);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Map<String, Object>> deleteQuestion(@PathVariable long id){
+        Map<String, Object> message = new HashMap<>();
+        try{
+            message.put("message", "question deleted");
+            questionService.delete(id);
+            return new ResponseEntity<>(message, HttpStatus.OK);
+        }catch(Exception e){
+            System.out.println("error:"+e.getMessage());
+            message.put("message", "cannot delete this question");
             return new ResponseEntity<>(message, HttpStatus.NOT_ACCEPTABLE);
         }
     }
--- src/main/java/com/youquiz/youquiz/Service/IMPL/QuestionService.java
+++ src/main/java/com/youquiz/youquiz/Service/IMPL/QuestionService.java
@@ -4,6 +4,7 @@ import com.youquiz.youquiz.DTO.QuestionDTO;
 import com.youquiz.youquiz.DTO.QuestionResponseDTO;
 import com.youquiz.youquiz.Entity.Question;
 import com.youquiz.youquiz.Enum.QuestionType;
+import com.youquiz.youquiz.Exceptions.NotFoundException;
 import com.youquiz.youquiz.Repository.LevelRepository;
 import com.youquiz.youquiz.Repository.QuestionRepository;
 import com.youquiz.youquiz.Repository.SubjectRepository;
@@ -39,4 +40,47 @@ public class QuestionService implements IQuestionService{
                 questionRepository.save(question), QuestionResponseDTO.class
         );
     }
+
+    @Override
+    public QuestionResponseDTO update(long id, QuestionDTO questionDTO) throws NotFoundException {
+        if(id <= 0)
+            throw new NotFoundException();
+        Question question = questionRepository.findById(id).get();
+        if(questionDTO.getQuestionText().isEmpty() == false)
+            question.setQuestionText(
+                    questionDTO.getQuestionText()
+            );
+        if(questionDTO.getCorrectResponseNbr() != 0)
+            question.setCorrectResponseNbr(
+                    questionDTO.getCorrectResponseNbr()
+            );
+        if(questionDTO.getResponseNbr() != 0)
+            question.setResponseNbr(
+                    questionDTO.getResponseNbr()
+            );
+        if(questionDTO.getTotalScore() != 0)
+            question.setTotalScore(
+                    questionDTO.getTotalScore()
+            );
+        if(questionDTO.getSubject_id() != 0)
+            question.setSubject(
+                    subjectRepository.findById(questionDTO.getSubject_id()).get()
+            );
+        if(questionDTO.getLevel_id() != 0)
+            question.setLevel(
+                    levelRepository.findById(questionDTO.getLevel_id()).get()
+            );
+        question.setType(
+                QuestionType.valueOf(questionDTO.getType())
+        );
+        return modelMapper.map(questionRepository.save(question), QuestionResponseDTO.class);
+    }
+
+    @Override
+    public void delete(long id) throws NotFoundException {
+        if(id <= 0)
+            throw new NotFoundException();
+        questionRepository.deleteById(id);
+    }
+
 }
